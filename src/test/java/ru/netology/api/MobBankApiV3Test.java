c840cb2b4aa281744bc40562ff2b168551184477
MG <gmelina69@gmail.com>
2026-04-29 22:23:36 +0400

    Уронить тест shouldMakeRequestBody()
    Проверить корректную работу СI

---
+++ src/test/java/ru/netology/api/MobBankApiV3Test.java
@@ -0,0 +1,43 @@
+package ru.netology.api;
+
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class MobBankApiV3Test {
+    private final RequestSpecification requestSpecification = new RequestSpecBuilder()
+            .setBaseUri("http://localhost")
+            .setBasePath("api/v1")
+            .setPort(9999)
+            .setAccept(ContentType.JSON)
+            .setContentType(ContentType.JSON)
+            .log(LogDetail.ALL)
+            .build();
+
+    @Test
+    public void shouldMakeRequest() {
+        given()
+                .spec(requestSpecification)
+                .when()
+                .get("/demo/accounts")
+                .then()
+                .statusCode(200);
+    }
+
+    @Test
+    public void shouldMakeRequestBody() {
+        given()
+                .spec(requestSpecification)
+                .when()
+                .get("/demo/accounts")
+                .then()
+                .body("[0].name", equalTo("Текущий счёт"))
+                .body("[1].balance", equalTo(2877210))
+                .body("[2].id", equalTo(2));
+    }
+}
