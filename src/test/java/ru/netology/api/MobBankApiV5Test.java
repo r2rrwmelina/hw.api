5c42f4baa077e5e3cb7cf7573659f4f05685c8e6
MG <gmelina69@gmail.com>
2026-04-29 22:34:03 +0400

    Уронить тест shouldMakeRequest()
    Изменить валидные значения в схеме accounts.schema.json для падения теста

---
+++ src/test/java/ru/netology/api/MobBankApiV5Test.java
@@ -0,0 +1,32 @@
+package ru.netology.api;
+
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+
+public class MobBankApiV5Test {
+    private final RequestSpecification requestSpec = new RequestSpecBuilder()
+            .setBaseUri("http://localhost")
+            .setBasePath("/api/v1")
+            .setPort(9999)
+            .setAccept(ContentType.JSON)
+            .setContentType(ContentType.JSON)
+            .log(LogDetail.ALL)
+            .build();
+
+    @Test
+    void shouldMakeRequest() {
+        given()
+                .spec(requestSpec)
+                .when()
+                .get("/demo/accounts")
+                .then()
+                .statusCode(200)
+                .body(matchesJsonSchemaInClasspath("accounts.schema.json"));
+    }
+}
